24ee61b4215cba38673a5ea217f4a6c87d4a461c
Kynan Song <kynan.song@bemo.co>
2018-06-22 14:39:41 +0100

    Adding export to androidbuild.sh

--- androidBuild.sh
+++ androidBuild.sh
@@ -4,6 +4,7 @@ set -e
 set -o pipefail
 
 cd /Users/kynan/.jenkins/workspace/AndroidBuildTest/Drinkable_app
+export JAVA_HOME=`/usr/libexec/java_home`
+export ANDROID_HOME=/Users/kynan/Library/Android/sdk
 ./gradlew assembleAndroidTest
 ./gradlew testDebugUnitTest
-
